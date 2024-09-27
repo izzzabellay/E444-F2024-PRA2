c58d299a9e0bfd1b582e2f51b8d7f470c33fb9b1
izzzabellay <izabella.yu@hotmail.com>
2024-09-27 13:34:35 -0400

    add bootstrap implementation

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,3 +13,5 @@ def user(name):
 
 if __name__=='__main__':
     app.run()
+    
+bootstrap = Bootstrap(app)
